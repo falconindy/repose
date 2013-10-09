e51145eaef28db09fa3a8b3f3b21b81c445f67dd
Simon Gomizelj <simongmzlj@gmail.com>
2013-10-09 02:50:19 -0400

    start adding license info

--- repose.c
+++ repose.c
@@ -1,3 +1,20 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) Simon Gomizelj, 2013
+ */
+
 #include "repose.h"
 
 #include <stdlib.h>
@@ -15,7 +32,6 @@
 #include "alpm/util.h"
 #include "alpm/base64.h"
 #include "database.h"
-#include "util.h"
 
 #define NOCOLOR     "\033[0m"
 #define BOLD        "\033[1m"
@@ -36,7 +52,36 @@
 #define BOLDMAGENTA "\033[1;35m"
 #define BOLDCYAN    "\033[1;36m"
 
-config_t cfg = {
+enum action {
+    ACTION_VERIFY,
+    ACTION_UPDATE,
+    ACTION_REMOVE,
+    ACTION_QUERY,
+    INVALID_ACTION
+};
+
+typedef struct colstr {
+    const char *colon;
+    const char *warn;
+    const char *error;
+    const char *nocolor;
+} colstr_t;
+
+static struct {
+    const char *key;
+    const char *arch;
+    char *pool;
+    enum action action;
+
+    short clean;
+    bool info;
+    bool sign;
+    bool files;
+    bool rebuild;
+
+    bool color;
+    colstr_t colstr;
+} cfg = {
     .action = INVALID_ACTION,
     .colstr = {
         .colon   = ":: ",
@@ -45,6 +90,21 @@ config_t cfg = {
         .nocolor = ""
     }
 };
+
+static int colon_printf(const char *fmt, ...)
+{
+    int ret;
+    va_list args;
+
+    va_start(args, fmt);
+    fputs(cfg.colstr.colon, stdout);
+    ret = vprintf(fmt, args);
+    fputs(cfg.colstr.nocolor, stdout);
+    va_end(args);
+
+    fflush(stdout);
+    return ret;
+}
 
 static void alloc_pkghash(repo_t *repo)
 {
@@ -61,19 +121,21 @@ static void alloc_pkghash(repo_t *repo)
     closedir(dirp);
 }
 
-static void populate_db_files(file_t *db, const char *name, const char *base, const char *ext)
+static int populate_db_files(file_t *db, const char *name, const char *base, const char *ext)
 {
-    /* precompile some filepaths:
-     *  - db full filename
-     *  - link to db file
-     *  - db signature
-     *  - link to signature
-     */
-
-    safe_asprintf(&db->file, "%s.%s%s", name, base, ext);
-    safe_asprintf(&db->sig, "%s.%s%s.sig", name, base, ext);
-    safe_asprintf(&db->link_file, "%s.%s", name, base);
-    safe_asprintf(&db->link_sig, "%s.%s.sig", name, base);
+    /* db full filename */
+    if (asprintf(&db->file, "%s.%s%s", name, base, ext) < 0)
+        return -1;
+    /* db signature */
+    if (asprintf(&db->sig, "%s.%s%s.sig", name, base, ext) < 0)
+        return -1;
+    /* link to db file */
+    if (asprintf(&db->link_file, "%s.%s", name, base) < 0)
+        return -1;
+    /* link to signature */
+    if (asprintf(&db->link_sig,  "%s.%s.sig", name, base) < 0)
+        return -1;
+    return 0;
 }
 
 static repo_t *repo_new(char *path)
@@ -109,51 +171,6 @@ static repo_t *repo_new(char *path)
         name = strndup(dbpath, dot - dbpath);
         repo->root = get_current_dir_name();
     }
-
-    if (!dot) {
-        errx(EXIT_FAILURE, "no file extension");
-    } else if (strcmp(dot, ".db") == 0) {
-        repo->compression = COMPRESS_GZIP;
-    } else if (strcmp(dot, ".db.tar") == 0) {
-        repo->compression = COMPRESS_NONE;
-    } else if (strcmp(dot, ".db.tar.gz") == 0) {
-        repo->compression = COMPRESS_GZIP;
-    } else if (strcmp(dot, ".db.tar.bz2") == 0) {
-        repo->compression = COMPRESS_BZIP2;
-    } else if (strcmp(dot, ".db.tar.xz") == 0) {
-        repo->compression = COMPRESS_XZ;
-    } else if (strcmp(dot, ".db.tar.Z") == 0) {
-        repo->compression = COMPRESS_COMPRESS;
-    } else {
-        errx(EXIT_FAILURE, "%s invalid repo type", dot);
-    }
-
-    /* open the directory so we can use openat later */
-    repo->rootfd = open(repo->root, O_RDONLY);
-    if (repo->poolfd < 0)
-        err(EXIT_FAILURE, "cannot access repo root %s", repo->root);
-
-    if (!repo->pool) {
-        repo->pool = repo->root;
-        repo->poolfd = repo->rootfd;
-    } else {
-        repo->poolfd = open(repo->pool, O_RDONLY);
-        if (repo->poolfd < 0)
-            err(EXIT_FAILURE, "cannot access repo pool %s", repo->pool);
-    }
-
-    alloc_pkghash(repo);
-
-    /* skip '.db' */
-    dot += 3;
-    if (*dot == '\0') {
-        dot = ".tar.gz";
-    }
-
-    populate_db_files(&repo->db, name, "db", dot);
-    if (cfg.files)
-        populate_db_files(&repo->files, name, "files", dot);
-
     if (cfg.rebuild) {
         repo->state = REPO_NEW;
     } else if (faccessat(repo->rootfd, repo->db.file, F_OK, 0) < 0) {
@@ -556,6 +573,317 @@ int main(int argc, char *argv[])
         rc = repo_database_verify(repo);
         break;
     case ACTION_UPDATE:
+        rc = repo_database_update(repo, argc - 1, argv + 1);
+        break;
+    case ACTION_REMOVE:
+        rc = repo_database_remove(repo, argc - 1, argv + 1);
+        break;
+    case ACTION_QUERY:
+        rc = repo_database_query(repo, argc - 1, argv + 1);
+        break;
+    default:
+        break;
+    };
+
+    return rc;
+}
+#include "repose.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <err.h>
+#include <errno.h>
+#include <dirent.h>
+#include <fnmatch.h>
+#include <sys/utsname.h>
+
+#include "alpm/signing.h"
+#include "alpm/util.h"
+#include "alpm/base64.h"
+#include "database.h"
+#include "util.h"
+
+#define NOCOLOR     "\033[0m"
+#define BOLD        "\033[1m"
+
+#define BLACK       "\033[0;30m"
+#define RED         "\033[0;31m"
+#define GREEN       "\033[0;32m"
+#define YELLOW      "\033[0;33m"
+#define BLUE        "\033[0;34m"
+#define MAGENTA     "\033[0;35m"
+#define CYAN        "\033[0;36m"
+
+#define BOLDBLACK   "\033[1;30m"
+#define BOLDRED     "\033[1;31m"
+#define BOLDGREEN   "\033[1;32m"
+#define BOLDYELLOW  "\033[1;33m"
+#define BOLDBLUE    "\033[1;34m"
+#define BOLDMAGENTA "\033[1;35m"
+#define BOLDCYAN    "\033[1;36m"
+
+config_t cfg = {
+    .action = INVALID_ACTION,
+    .colstr = {
+        .colon   = ":: ",
+        .warn    = "",
+        .error   = "",
+        .nocolor = ""
+    }
+};
+
+static void alloc_pkghash(repo_t *repo)
+{
+    DIR *dirp = opendir(repo->pool);
+    struct dirent *entry;
+
+    repo->cachesize = 0;
+    while ((entry = readdir(dirp)) != NULL) {
+        if (entry->d_type == DT_REG)
+            repo->cachesize++;
+    }
+
+    repo->pkgcache = _alpm_pkghash_create(repo->cachesize);
+    closedir(dirp);
+}
+
+static void populate_db_files(file_t *db, const char *name, const char *base, const char *ext)
+{
+    /* precompile some filepaths:
+     *  - db full filename
+     *  - link to db file
+     *  - db signature
+     *  - link to signature
+     */
+
+    safe_asprintf(&db->file, "%s.%s%s", name, base, ext);
+    safe_asprintf(&db->sig, "%s.%s%s.sig", name, base, ext);
+    safe_asprintf(&db->link_file, "%s.%s", name, base);
+    safe_asprintf(&db->link_sig, "%s.%s.sig", name, base);
+}
+
+static repo_t *repo_new(char *path)
+{
+    size_t len;
+    char *div, *dot, *name;
+    char *dbpath = NULL, *real = NULL;
+
+    real = realpath(path, NULL);
+    if (real) {
+        dbpath = real;
+    } else {
+        if (errno != ENOENT)
+            err(EXIT_FAILURE, "failed to find repo");
+        dbpath = path;
+    }
+
+    repo_t *repo = malloc(sizeof(repo_t));
+    *repo = (repo_t){
+        .state = REPO_CLEAN,
+        .pool  = cfg.pool
+    };
+
+    len = strlen(dbpath);
+    div = memrchr(dbpath, '/', len);
+
+    if (div) {
+        dot = memchr(div, '.', len - (dbpath - div));
+        name = strndup(div + 1, dot - div - 1);
+        repo->root = strndup(dbpath, div - dbpath);
+    } else {
+        dot = memchr(dbpath, '.', len);
+        name = strndup(dbpath, dot - dbpath);
+        repo->root = get_current_dir_name();
+    }
+
+    if (!dot) {
+        errx(EXIT_FAILURE, "no file extension");
+    } else if (strcmp(dot, ".db") == 0) {
+        repo->compression = COMPRESS_GZIP;
+    } else if (strcmp(dot, ".db.tar") == 0) {
+        repo->compression = COMPRESS_NONE;
+    } else if (strcmp(dot, ".db.tar.gz") == 0) {
+        repo->compression = COMPRESS_GZIP;
+    } else if (strcmp(dot, ".db.tar.bz2") == 0) {
+        repo->compression = COMPRESS_BZIP2;
+    } else if (strcmp(dot, ".db.tar.xz") == 0) {
+        repo->compression = COMPRESS_XZ;
+    } else if (strcmp(dot, ".db.tar.Z") == 0) {
+        repo->compression = COMPRESS_COMPRESS;
+    } else {
+        errx(EXIT_FAILURE, "%s invalid repo type", dot);
+    }
+
+    /* open the directory so we can use openat later */
+    repo->rootfd = open(repo->root, O_RDONLY);
+    if (repo->poolfd < 0)
+        err(EXIT_FAILURE, "cannot access repo root %s", repo->root);
+
+    if (!repo->pool) {
+        repo->pool = repo->root;
+        repo->poolfd = repo->rootfd;
+    } else {
+        repo->poolfd = open(repo->pool, O_RDONLY);
+        if (repo->poolfd < 0)
+            err(EXIT_FAILURE, "cannot access repo pool %s", repo->pool);
+    }
+
+    alloc_pkghash(repo);
+
+    /* skip '.db' */
+    dot += 3;
+    if (*dot == '\0') {
+        dot = ".tar.gz";
+    }
+
+    populate_db_files(&repo->db, name, "db", dot);
+    if (cfg.files)
+        populate_db_files(&repo->files, name, "files", dot);
+
+    if (cfg.rebuild) {
+        repo->state = REPO_NEW;
+    } else if (faccessat(repo->rootfd, repo->db.file, F_OK, 0) < 0) {
+        if (errno != ENOENT) {
+            err(EXIT_FAILURE, "couldn't access database %s", repo->db.file);
+        }
+        repo->state = REPO_NEW;
+    } else {
+        colon_printf("Reading existing database into memory...\n");
+
+        /* load the databases if possible */
+        load_database(repo, &repo->db);
+        if (cfg.files) {
+            load_database(repo, &repo->files);
+        }
+
+        repo_database_reduce(repo);
+    }
+
+    free(name);
+    free(real);
+    return repo;
+}
+
+int repo_write(repo_t *repo)
+{
+    switch (repo->state) {
+    case REPO_NEW:
+        printf("repo empty!\n");
+        return 1;
+    case REPO_CLEAN:
+        printf("repo does not need updating\n");
+        break;
+    case REPO_DIRTY:
+        colon_printf("Writing databases to disk...\n");
+        printf("writing %s...\n", repo->db.file);
+        compile_database(repo, &repo->db, DB_DESC | DB_DEPENDS);
+        if (cfg.sign) {
+            sign_database(repo, &repo->db, cfg.key);
+        }
+
+        if (cfg.files) {
+            printf("writing %s...\n", repo->files.file);
+            compile_database(repo, &repo->files, DB_FILES);
+            if (cfg.sign) {
+                sign_database(repo, &repo->db, cfg.key);
+            }
+        }
+        printf("repo updated successfully\n");
+        break;
+    default:
+        break;
+    }
+
+    return 0;
+}
+
+int unlink_package(repo_t *repo, const alpm_pkg_meta_t *pkg)
+{
+    if (faccessat(repo->poolfd, pkg->filename, F_OK, 0) < 0) {
+        if (errno != ENOENT) {
+            err(EXIT_FAILURE, "couldn't access package %s to unlink", pkg->filename);
+        }
+        return 0;
+    }
+
+    printf("deleting %s %s\n", pkg->name, pkg->version);
+    unlinkat(repo->poolfd, pkg->filename, 0);
+    unlinkat(repo->poolfd, pkg->signame, 0);
+    return 0;
+}
+
+static alpm_pkg_meta_t *load_package(repo_t *repo, const char *filename)
+{
+    alpm_pkg_meta_t *pkg = NULL;
+    int sigfd, pkgfd = openat(repo->poolfd, filename, O_RDONLY);
+    if (pkgfd < 0) {
+        err(EXIT_FAILURE, "failed to open %s", filename);
+    }
+
+    alpm_pkg_load_metadata(pkgfd, &pkg);
+
+    if (strcmp(pkg->arch, cfg.arch) != 0 && strcmp(pkg->arch, "any") != 0) {
+        alpm_pkg_free_metadata(pkg);
+        return NULL;
+    }
+    pkg->filename = strdup(filename);
+
+    safe_asprintf(&pkg->signame, "%s.sig", filename);
+
+    sigfd = openat(repo->poolfd, pkg->signame, O_RDONLY);
+    if (sigfd < 0) {
+        if (errno != ENOENT)
+            err(EXIT_FAILURE, "failed to open %s", pkg->signame);
+    } else {
+        read_pkg_signature(sigfd, pkg);
+    }
+
+    return pkg;
+}
+
+static inline alpm_pkghash_t *pkgcache_add(alpm_pkghash_t *cache, alpm_pkg_meta_t *pkg)
+{
+    alpm_pkg_meta_t *old = _alpm_pkghash_find(cache, pkg->name);
+    int vercmp = old == NULL ? 0 : alpm_pkg_vercmp(pkg->version, old->version);
+
+    if (vercmp == 0 || vercmp == 1) {
+        if (old) {
+            cache = _alpm_pkghash_remove(cache, old, NULL);
+            /* if (cfg.clean >= 2) */
+            /*     unlink_package(repo, old); */
+            alpm_pkg_free_metadata(old);
+        }
+        return _alpm_pkghash_add(cache, pkg);
+    } else {
+        /* if (cfg.clean >= 2) */
+        /*     unlink_package(repo, pkg); */
+        alpm_pkg_free_metadata(pkg);
+        return cache;
+    }
+}
+
+static inline alpm_pkghash_t *pkgcache_from_file(alpm_pkghash_t *cache, repo_t *repo, const char *filepath)
+{
+    alpm_pkg_meta_t *pkg;
+    const char *basename = strrchr(filepath, '/');
+
+    if (basename) {
+        if (memcmp(filepath, repo->pool, basename - filepath) != 0) {
+            warnx("%s is not in the same path as the database", filepath);
+            return cache;
+        }
+    }
+
+    pkg = load_package(repo, filepath);
+    if (!pkg)
+        return cache;
+
+    return pkgcache_add(cache, pkg);
+}
+
         rc = repo_database_update(repo, argc - 1, argv + 1);
         break;
     case ACTION_REMOVE:
